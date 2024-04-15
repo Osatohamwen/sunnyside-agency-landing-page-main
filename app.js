cb5e156b72861a18d96f5a9abd5c5352d73f4b8a
Osatohamwen <162594392+Osatohamwen@users.noreply.github.com>
2024-04-15 06:46:50 +0100

    JS updated commit

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ menuIcon.addEventListener('click',(e) => {
     menuActiveIcon.style.display = "flex";
     menuIcon.style.display = "none";
     navItems.style.display = "flex";
-    menuClose.style.display = "flex";
+    menuClose.style.display = "inline-flex";
 });
 
 menuClose.addEventListener('click',(e) => {
     menuActiveIcon.style.display = "none";
-    menuIcon.style.display = "flex";
+    menuIcon.style.display = "inline-flex";
     navItems.style.display = "none";
     menuClose.style.display = "none";
 });
