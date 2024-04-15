f2346540516c014d28536e6d36b18162f97530f7
Osatohamwen <162594392+Osatohamwen@users.noreply.github.com>
2024-04-15 05:48:46 +0100

    final commit

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const navItems = document.getElementById("navitems")
 const menuIcon = document.getElementById("hamburger")
+const menuClose = document.getElementById("hamburger-close")
 const menuActiveIcon = document.getElementById("triangle")
 
 menuIcon.addEventListener('click',(e) => {
     menuActiveIcon.style.display = "flex";
-    menuIcon.style.display = "inline-flex";
+    menuIcon.style.display = "none";
     navItems.style.display = "flex";
+    menuClose.style.display = "inline-flex"
 });
 
-// menuIcon.addEventListener('click',(e) => {
-//     menuActiveIcon.style.display = "none";
-//     menuIcon.style.display = "inline-flex";
-//     navItems.style.display = "none";
-// });
+menuClose.addEventListener('click',(e) => {
+    menuActiveIcon.style.display = "none";
+    menuIcon.style.display = "inline-flex";
+    navItems.style.display = "none";
+    menuClose.style.display = "none"
+});
